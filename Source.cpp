8fafafc123d554ae8ebe756b972a0826aca86420
mixedfeelings13 <fuegodjh@gmail.com>
2023-10-11 12:08:30 +0100

    Version 1

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+int main () {
+
+  // Create Screen Buffer
+  wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+  HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+  SetConsoleActiveScreenBuffer(hConsole);
+  DWORD dwBytesWritten = 0;
+
+  return 0;
+}
